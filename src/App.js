919e87b6a12ab6e0f2e32dcebcb2690728194bfa
Snir Dahan <snirdahan01@gmail.com>
2021-08-13 17:43:19 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/homepage/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signIn' component={SignInAndSignUpPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directroy.component';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
